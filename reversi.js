bf7aaf571d9d9a1d638aa51d8f6ecdfef8f76284
sumitaka-sakuma <tksn.playthe69gt2@gmail.com>
2020-07-11 00:32:35 +0900

    expanded the board

--- reversi.js
+++ reversi.js
@@ -2,6 +2,19 @@ console.log('hello')
 
 window.onload = () => {
 
+
+    const rows = Array.from({length: 8}, (v,k) => k+1)
+    const columns = Array.from({length: 8}, (v,k) => k+1)
+
+    for(row of rows){
+        for(column of columns){
+            document.querySelector('.grid-container').insertAdjacentHTML(
+                'beforeend',
+                `<div class="grid-item" data-row ="${row}" data-column="${column}"></div>`
+            )
+        }
+    }
+
     Array.from(document.getElementsByClassName('grid-item')).forEach(element => {
         element.addEventListener('click', (event) => {
             event.target.dataset.color = "white"
